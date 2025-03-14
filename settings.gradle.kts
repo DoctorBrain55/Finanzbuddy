f1a7aaaf7ddded8e15d83c4f592089f41a5b79f5
DoctorBrain55 <s_sevincs22@stud.hwr-berlin.de>
2025-03-14 13:10:31 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,24 +1,30 @@
+// Plugin-Management: Definiert die Quellen für Plugins
 pluginManagement {
     repositories {
         google {
             content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
+                includeGroupByRegex("com\\.android.*") // Enthält Android-spezifische Plugins
+                includeGroupByRegex("com\\.google.*") // Enthält Google-Dienste (z. B. Firebase)
+                includeGroupByRegex("androidx.*") // Enthält AndroidX-Bibliotheken
             }
         }
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://jitpack.io") } // Use uri() function
+        mavenCentral() // Fügt das Maven Central Repository hinzu
+        gradlePluginPortal() // Enthält Gradle-Plugins
     }
 }
 
+// Verwaltung der Abhängigkeitsauflösung
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS) // Verhindert das Überschreiben der Repositories durch Unterprojekte
+    repositories {
+        google() // Standard-Repository für Android-Bibliotheken
+        mavenCentral() // Offizielles Maven-Repository für Java- und Android-Bibliotheken
+        maven { url = uri("https://jitpack.io") } // Fügt JitPack hinzu, um Drittanbieter-Bibliotheken zu nutzen
+    }
+}
+
+// Definiert den Namen des Root-Projekts
 rootProject.name = "FinanzBuddy"
+
+// Fügt das App-Modul zur Projektstruktur hinzu
 include(":app")
